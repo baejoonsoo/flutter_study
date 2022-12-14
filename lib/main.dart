fa7fa9f6b424d0b6e962cf3f74d02d7a5b9d1790
baejoonsoo <artns25@dsm.hs.kr>
2022-12-14 16:19:35 +0900

    camera v2

--- lib/main.dart
+++ lib/main.dart
@@ -11,107 +11,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Container(
-            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-            child: Row(
-              children: const [
-                Text(
-                  "금호가 3가",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+          appBar: AppBar(
+            title: Container(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Row(
+                children: const [
+                  Text(
+                    "금호가 3가",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.keyboard_arrow_down,
+                  Icon(
+                    Icons.keyboard_arrow_down,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
                   color: Colors.black,
                 ),
-              ],
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: Colors.black,
               ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black,
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications_none,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        body: SizedBox(
-          width: double.infinity,
-          height: 150,
-          child: Row(
-            children: [
-              Image.asset(
-                "assets/camera.png",
-                width: 150,
-                height: 150,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      width: 200,
-                      child: Text(
-                        "캐논 DSLR 1000D(단렌즈, 충전기 16기가 SD 포함)",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    const Text(
-                      "성동구 행당동 ∙ 끝올 10분 전",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
-                    const Text(
-                      "210,000원",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.favorite_border_outlined,
-                          color: Colors.grey,
-                        ),
-                        Text("4")
-                      ],
-                    )
-                  ],
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.black,
                 ),
-              )
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_none,
+                  color: Colors.black,
+                ),
+              ),
             ],
           ),
-        ),
-      ),
+          body: Container(
+            height: 150,
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/camera.png",
+                  width: 150,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        "카메라 팝니다.카메라 팝니다.카메라 팝니다.카메라 팝니다.카메라 팝니다.카메라 팝니다.",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const Text("금호동 3가"),
+                      const Text("7000원"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Icon(Icons.favorite_border),
+                          Text('4'),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
